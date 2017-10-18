9b7a709fc0a89395cb318945679753d21a136f36
Alexander Condello <acondello@dwavesys.com>
2017-10-18 16:23:54 -0700

    a fair ways down the networkx direction, not sure it's a great idea...

--- dwave_maxgap/generation.py
+++ dwave_maxgap/generation.py
@@ -3,98 +3,112 @@ Generation of max-gap penalty models using pysmt.
 """
 import itertools
 
+import networkx as nx
+import dwave_networkx as dnx
+
 from pysmt.shortcuts import Symbol, GE, LE, Real, Plus, Equals, And
 from pysmt.shortcuts import Solver
 from pysmt.typing import REAL
 
 from dwave_maxgap.penalty_model import PenaltyModel
 
-__all__ = ['generate_small_no_aux']
+__all__ = ['generate_ising_no_aux', 'generate_ising']
 
 
-class _Theta(object):
+class Theta(nx.Graph):
     """Encodes the smt variables
-
-    Examples:
-        >>> theta = _Theta(nx.complete_graph(2),
-        ...                {0: (-2., 2.), 1: (-2., 2)}, {(0, 1): (-1., 1.)})
     """
-    def __init__(self, graph, linear_energy_ranges, quadratic_energy_ranges):
+    def __init__(self):
+        nx.Graph.__init__(self)
+        self.assertions = set()
+        self.offset = Symbol('offset', REAL)
 
-        # we want to store all of the smt assertions in a set
-        self.assertions = assertions = set()
+    def add_linear_biases_from(self, nodes, energy_ranges):
 
-        # need a stable edgelist
-        self.edgelist = edgelist = list(graph.edges())
+        assertions = self.assertions
 
-        # next we need to nodes/linear biases
         def linear_bias(v):
             bias = Symbol('h_{}'.format(v), REAL)
 
-            min_, max_ = linear_energy_ranges[v]
+            min_, max_ = energy_ranges[v]
 
             assertions.add(LE(bias, Real(max_)))
             assertions.add(GE(bias, Real(min_)))
 
             return bias
 
-        self.linear = {v: linear_bias(v) for v in graph}
+        self.add_nodes_from((v, {'bias': linear_bias(v)}) for v in nodes)
 
-        self.adjacency = adjacency = {v: dict() for v in graph}
+    def add_quadratic_biases_from(self, edges, energy_ranges):
+
+        assertions = self.assertions
 
         def quadratic_bias(u, v):
             bias = Symbol('J_{},{}'.format(u, v), REAL)
 
-            if (v, u) in quadratic_energy_ranges:
-                min_, max_ = quadratic_energy_ranges[(v, u)]
+            if (v, u) in energy_ranges:
+                min_, max_ = energy_ranges[(v, u)]
             else:
-                min_, max_ = quadratic_energy_ranges[(u, v)]
-
-            adjacency[u][v] = bias
-            adjacency[v][u] = bias
+                min_, max_ = energy_ranges[(u, v)]
 
             assertions.add(LE(bias, Real(max_)))
             assertions.add(GE(bias, Real(min_)))
 
             return bias
 
-        self.quadratic = {(u, v): quadratic_bias(u, v) for u, v in edgelist}
+        self.add_edges_from((u, v, {'bias': quadratic_bias(u, v)}) for u, v in edges)
 
-        self.offset = Symbol('offset', REAL)
+    def fix_variables(self, values):
+        subtheta = self.subgraph(v for v in self if v not in values)
+        subtheta.offset = self.offset
 
-    # def __getitem__(self, v):
-    #     return self._quadratic[v]
+        # now we need to add the eliminated values to the offset or linear
+        # biases
+        for v, val in values.items():
+            subtheta.offset = Plus(Real(val) * self.nodes[v]['bias'], subtheta.offset)
 
-    # def quadratic(self):
-    #     for u, v in itertools.combinations(list(self._linear), 2):
-    #         if v in self._quadratic[u]
-    #         yield u, v, self._quadratic[u][v]
+        for u, v in self.edges:
+            if u in values and v in values:
+                subtheta.offset = Plus(Real(values[u] * values[v]) * self[u][v]['bias'], subtheta.offset)
+            elif u in values:
+                pass
 
-    # def linear(self):
-    #     return ((v, bias) for v, bias in self._linear.items())
+
+        print(subtheta.offset)
+
+
+class Table(object):
+    """TODO"""
+    def __init__(self, graph, decision_variables, theta):
+        aux_subgraph = graph.subgraph(v for v in graph if v not in decision_variables)
+        __, order = dnx.treewidth_branch_and_bound(aux_subgraph)
+
+        self.theta = theta
+
+    def energy_upperbound(self, values):
+
+        subtheta = self.theta.fix_variables(values)
+
+        # now we need to add the elimina
 
 
 def generate_ising_no_aux(graph, configurations, decision_variables,
-                          linear_energy_ranges=None, quadratic_energy_ranges=None):
+                          linear_energy_ranges, quadratic_energy_ranges):
     """TODO"""
 
-    # this only works for models with no auxiliary variables and less than
-    # 4 nodes
+    # this only works for models with no auxiliary variables
     num_nodes = len(graph)
 
     if len(decision_variables) != num_nodes:
         raise ValueError("every node must correspond to a decision variable")
 
-    if linear_energy_ranges is None:
-        linear_energy_ranges = {v: (-2., 2.) for v in graph}
-    if quadratic_energy_ranges is None:
-        quadratic_energy_ranges = {(u, v): (-1., 1.) for u, v in graph.edges}
-
     # ok, this thing is simple so we can just use exact energies for each
     # configuration
 
     # need the smt variables
-    theta = _Theta(graph, linear_energy_ranges, quadratic_energy_ranges)
+    theta = Theta()
+    theta.add_linear_biases_from(graph, linear_energy_ranges)
+    theta.add_quadratic_biases_from(graph.edges(), quadratic_energy_ranges)
 
     assertions = set()
     gap = Symbol('gap', REAL)
@@ -104,9 +118,9 @@ def generate_ising_no_aux(graph, configurations, decision_variables,
         # get the exact energy for the configuration
         spins = dict(zip(decision_variables, config))
 
-        linear_energy = Plus([Real(spins[v]) * bias for v, bias in theta.linear.items()])
-        quadratic_energy = Plus([Real(spins[u] * spins[v]) * bias
-                                 for (u, v), bias in theta.quadratic.items()])
+        linear_energy = Plus([Real(spins[v]) * theta.nodes[v]['bias'] for v in theta])
+        quadratic_energy = Plus([Real(spins[u] * spins[v]) * theta[u][v]['bias']
+                                 for u, v in theta.edges])
         energy = Plus([linear_energy, quadratic_energy, theta.offset])
 
         if config in configurations:
@@ -131,9 +145,39 @@ def generate_ising_no_aux(graph, configurations, decision_variables,
     # finally we need to convert our values back into python floats.
     # we use limit_denominator to deal with some of the rounding
     # issues.
-    h = {v: float(model.get_py_value(bias).limit_denominator())
-         for v, bias in theta.linear.items()}
-    J = {edge: float(model.get_py_value(bias).limit_denominator())
-         for edge, bias in theta.quadratic.items()}
+    h = {v: float(model.get_py_value(theta.nodes[v]['bias']).limit_denominator())
+         for v in theta}
+    J = {(u, v): float(model.get_py_value(theta[u][v]['bias']).limit_denominator())
+         for u, v in theta.edges}
+    offset = float(model.get_py_value(theta.offset).limit_denominator())
+    gap = float(model.get_py_value(gap).limit_denominator())
 
-    return h, J
+    return h, J, offset, gap
+
+
+def generate_ising(graph, configurations, decision_variables,
+                   linear_energy_ranges, quadratic_energy_ranges):
+    """TODO"""
+
+    num_nodes = len(graph)
+
+    # need the smt variables
+    theta = Theta()
+    theta.add_linear_biases_from(graph, linear_energy_ranges)
+    theta.add_quadratic_biases_from(graph.edges(), quadratic_energy_ranges)
+
+    # we need to build a table of messages
+    table = Table(graph, decision_variables, theta)
+
+    gap = Symbol('gap', REAL)
+    for config in itertools.product((-1, 1), repeat=num_nodes):
+
+        # get the exact energy for the configuration
+        spins = dict(zip(decision_variables, config))
+
+        if config in configurations:
+            energy = table.energy(spins)
+            assertions.add(Equals(energy, Real(0.0)))
+        else:
+            energy = table.energy_upperbound(spins)
+            assertions.add(GE(energy, gap))
--- dwave_maxgap/tests/test_generation.py
+++ dwave_maxgap/tests/test_generation.py
@@ -1,14 +1,40 @@
 import unittest
 
 import networkx as nx
+import dwave_networkx as dnx
 
 import dwave_maxgap as maxgap
 
 
-class Test_maxgap_small_no_aux(unittest.TestCase):
+class TestNoAux(unittest.TestCase):
     def test_basic(self):
         graph = nx.complete_graph(4)
 
         configurations = {(-1, -1, -1, -1), (1, 1, 1, 1)}
 
-        pm = maxgap.generate_small_no_aux(graph, configurations, list(graph))
+        linear_energy_ranges = {v: (-2., 2.) for v in graph}
+        quadratic_energy_ranges = {(u, v): (-1., 1.) for u, v in graph.edges}
+
+        h, J, offset, gap = maxgap.generate_ising_no_aux(graph, configurations, list(graph),
+                                                         linear_energy_ranges,
+                                                         quadratic_energy_ranges)
+
+        # TODO, something interesting here
+
+
+class TestGeneration(unittest.TestCase):
+    def test_basic(self):
+        graph = dnx.chimera_graph(1)
+
+        configurations = {(-1, -1, -1),
+                          (1, 1, 1),
+                          (-1, 1, -1),
+                          (1, -1, -1)}
+
+        linear_energy_ranges = {v: (-2., 2.) for v in graph}
+        quadratic_energy_ranges = {(u, v): (-1., 1.) for u, v in graph.edges}
+
+        h, J, offset, gap = maxgap.generate_ising(graph, configurations,
+                                                  (0, 1, 2),
+                                                  linear_energy_ranges,
+                                                  quadratic_energy_ranges)
