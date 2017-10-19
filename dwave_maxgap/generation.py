3cf8f3a8794f4f8d59a8eb10d4d59afda7ebccc0
Alexander Condello <acondello@dwavesys.com>
2017-10-19 12:50:03 -0700

    first pass is not working

--- dwave_maxgap/generation.py
+++ dwave_maxgap/generation.py
@@ -7,13 +7,13 @@ import networkx as nx
 import dwave_networkx as dnx
 from six import iteritems
 
-from pysmt.shortcuts import Symbol, GE, LE, Real, Plus, Equals, And
+from pysmt.shortcuts import Symbol, FreshSymbol, GE, LE, Real, Plus, Equals, And, Times, Ite
 from pysmt.shortcuts import Solver
-from pysmt.typing import REAL
+from pysmt.typing import REAL, BOOL
 
 from dwave_maxgap.penalty_model import PenaltyModel
 
-__all__ = ['generate_ising_no_aux', 'generate_ising']
+__all__ = ['generate_ising']
 
 
 class Theta(object):
@@ -90,18 +90,18 @@ class Theta(object):
         # gets added to the offset, otherwise it gets added to subtheta
         for v, bias in iteritems(self.linear):
             if v in spins:
-                subtheta.offset = Plus(subtheta.offset, Real(spins[v]) * bias)
+                subtheta.offset = Plus(subtheta.offset, Times(Real(spins[v]), bias))
             else:
                 subtheta.linear[v] = bias
 
         # and now the quadratic biases get allocated.
         for (u, v), bias in iteritems(self.quadratic):
             if u in spins and v in spins:
-                subtheta.offset = Plus(subtheta.offset, Real(spins[v] * spins[u]) * bias)
+                subtheta.offset = Plus(subtheta.offset, Times(Real(spins[v] * spins[u]), bias))
             elif u in spins:
-                subtheta.linear[v] = Plus(subtheta.linear[v], Real(spins[u]) * bias)
+                subtheta.linear[v] = Plus(subtheta.linear[v], Times(Real(spins[u]), bias))
             elif v in spins:
-                subtheta.linear[u] = Plus(subtheta.linear[u], Real(spins[v]) * bias)
+                subtheta.linear[u] = Plus(subtheta.linear[u], Times(Real(spins[v]), bias))
             else:
                 subtheta.quadratic[(u, v)] = bias
 
@@ -124,55 +124,161 @@ class Table(object):
     """TODO"""
     def __init__(self, graph, decision_variables, theta):
         aux_subgraph = graph.subgraph(v for v in graph if v not in decision_variables)
-        __, order = dnx.treewidth_branch_and_bound(aux_subgraph)
+        __, self.order = dnx.treewidth_branch_and_bound(aux_subgraph)
 
         self.theta = theta
+
+        self.assertions = set()
+
+        self.fresh_auxvar = 0  # let's us make fresh aux variables
 
     def energy_upperbound(self, values):
 
         subtheta = self.theta.fix_variables(values)
 
-        # now we need to add the elimina
-        raise NotImplementedError
+        # ok, let's start eliminating variables
+        order = list(self.order)
+
+        if order:
+            return self.message_upperbound(order, {}, subtheta)
+        else:
+            # if there are no variables to eliminate, then the offset of
+            # subtheta is the exact value and we can just return it
+            assert not subtheta.linear and not subtheta.quadratic
+            return subtheta.offset
+
+    def energy(self, values):
+
+        subtheta = self.theta.fix_variables(values)
+
+        # we need aux variables
+        av = self.fresh_auxvar
+        auxvars = {v: Symbol('aux{}_{}'.format(av, v), BOOL) for v in subtheta.linear}
+        self.fresh_auxvar += 1
+
+        # ok, let's start eliminating variables
+        order = list(self.order)
+
+        if order:
+            return self.message(order, {}, subtheta, auxvars)
+        else:
+            # if there are no variables to eliminate, then the offset of
+            # subtheta is the exact value and we can just return it
+            assert not subtheta.linear and not subtheta.quadratic
+            return subtheta.offset
+
+    def message(self, order, spins, subtheta, auxvars):
+        # get the last variable in the elimination order
+        v = order.pop()
+        aux = auxvars[v]
+
+        # build an iterable over all of the energies contributions
+        # that we can exactly determine given v and our known spins
+        # in these contributions we assume that v is positive
+        def energy_contributions():
+            yield subtheta.linear[v]
+
+            for u, bias in iteritems(subtheta.adj[v]):
+                if u in spins:
+                    yield Times(Real(spins[u]), bias)
+
+        energy = Plus(energy_contributions())
+
+        # if there are no more variables in the order, we can stop
+        # otherwise we need the next message variable
+        if order:
+            spins[v] = 1.0
+            plus = self.message(order, spins, subtheta, auxvars)
+            spins[v] = -1.0
+            minus = self.message(order, spins, subtheta, auxvars)
+            del spins[v]
+        else:
+            plus = minus = Real(0.0)
+
+        # we now need a real-valued smt variable to be our message
+        m = FreshSymbol(REAL)
+
+        self.assertions.update({LE(m, Plus(energy, plus)),
+                                LE(m, Plus(Times(energy, Real(-1.)), minus)),
+                                Ite(aux,
+                                    GE(m, Plus(energy, plus)),
+                                    GE(m, Plus(Times(energy, Real(-1.)), minus)))
+                                })
+
+        order.append(v)
+
+        return m
+
+    def message_upperbound(self, order, spins, subtheta):
+
+        # get the last variable in the elimination order
+        v = order.pop()
+
+        # build an iterable over all of the energies contributions
+        # that we can exactly determine given v and our known spins
+        # in these contributions we assume that v is positive
+        def energy_contributions():
+            yield subtheta.linear[v]
+
+            for u, bias in iteritems(subtheta.adj[v]):
+                if u in spins:
+                    yield Times(Real(spins[u]), bias)
+
+        energy = Plus(energy_contributions())
+
+        # if there are no more variables in the order, we can stop
+        # otherwise we need the next message variable
+        if order:
+            spins[v] = 1.
+            plus = self.message_upperbound(order, spins, subtheta)
+            spins[v] = -1.
+            minus = self.message_upperbound(order, spins, subtheta)
+            del spins[v]
+        else:
+            plus = minus = Real(0.0)
+
+        # we now need a real-valued smt variable to be our message
+        m = FreshSymbol(REAL)
+
+        self.assertions.update({LE(m, Plus(energy, plus)),
+                                LE(m, Plus(Times(energy, Real(-1.)), minus))})
+
+        order.append(v)
+
+        return m
 
 
-def generate_ising_no_aux(graph, configurations, decision_variables,
-                          linear_energy_ranges, quadratic_energy_ranges):
+def generate_ising(graph, configurations, decision_variables,
+                   linear_energy_ranges, quadratic_energy_ranges):
     """TODO"""
+    pass
 
-    # this only works for models with no auxiliary variables
     num_nodes = len(graph)
+    num_variables = len(decision_variables)
 
-    if len(decision_variables) != num_nodes:
-        raise ValueError("every node must correspond to a decision variable")
-
-    # ok, this thing is simple so we can just use exact energies for each
-    # configuration
-
-    # need the smt variables
+    # need the smt variables for the biases
     theta = Theta(graph, linear_energy_ranges, quadratic_energy_ranges)
+
+    # we need to build a table of messages
+    table = Table(graph, decision_variables, theta)
 
     assertions = set()
     gap = Symbol('gap', REAL)
-    # ok, let's start iterating through all configs!
-    for config in itertools.product((-1, 1), repeat=num_nodes):
-
+    for config in itertools.product((-1, 1), repeat=num_variables):
         # get the exact energy for the configuration
         spins = dict(zip(decision_variables, config))
 
-        linear_energy = Plus([Real(spins[v]) * bias for v, bias in iteritems(theta.linear)])
-        quadratic_energy = Plus([Real(spins[u] * spins[v]) * bias
-                                 for (u, v), bias in iteritems(theta.quadratic)])
-        energy = Plus([linear_energy, quadratic_energy, theta.offset])
-
         if config in configurations:
+            energy = table.energy(spins)
             assertions.add(Equals(energy, Real(0.0)))
         else:
+            energy = table.energy_upperbound(spins)
             assertions.add(GE(energy, gap))
 
     assertions.update(theta.assertions)
-    # ok, problem is set up, let's get solving
+    assertions.update(table.assertions)
 
+    # ok, problem is set up, let's get solving
     with Solver() as solver:
         solver.add_assertion(And(assertions))
 
@@ -195,30 +301,3 @@ def generate_ising_no_aux(graph, configurations, decision_variables,
     gap = float(model.get_py_value(gap).limit_denominator())
 
     return h, J, offset, gap
-
-
-def generate_ising(graph, configurations, decision_variables,
-                   linear_energy_ranges, quadratic_energy_ranges):
-    """TODO"""
-    pass
-
-    num_nodes = len(graph)
-
-    # need the smt variables for the biases
-    theta = Theta(graph, linear_energy_ranges, quadratic_energy_ranges)
-
-    # we need to build a table of messages
-    table = Table(graph, decision_variables, theta)
-
-    gap = Symbol('gap', REAL)
-    for config in itertools.product((-1, 1), repeat=num_nodes):
-
-        # get the exact energy for the configuration
-        spins = dict(zip(decision_variables, config))
-
-        if config in configurations:
-            energy = table.energy(spins)
-            assertions.add(Equals(energy, Real(0.0)))
-        else:
-            energy = table.energy_upperbound(spins)
-            assertions.add(GE(energy, gap))
--- dwave_maxgap/tests/test_generation.py
+++ dwave_maxgap/tests/test_generation.py
@@ -5,31 +5,20 @@ import dwave_networkx as dnx
 
 import dwave_maxgap as maxgap
 
-
-class TestNoAux(unittest.TestCase):
-    def test_basic(self):
-        graph = nx.complete_graph(4)
-
-        configurations = {(-1, -1, -1, -1), (1, 1, 1, 1)}
-
-        linear_energy_ranges = {v: (-2., 2.) for v in graph}
-        quadratic_energy_ranges = {(u, v): (-1., 1.) for u, v in graph.edges}
-
-        h, J, offset, gap = maxgap.generate_ising_no_aux(graph, configurations, list(graph),
-                                                         linear_energy_ranges,
-                                                         quadratic_energy_ranges)
-
-        # TODO, something interesting here
+from pysmt.environment import get_env, reset_env
 
 
 class TestGeneration(unittest.TestCase):
+    def setUp(self):
+        self.env = reset_env()
+
     def test_basic(self):
         graph = dnx.chimera_graph(1)
 
         configurations = {(-1, -1, -1),
-                          (1, 1, 1),
-                          (-1, 1, -1),
-                          (1, -1, -1)}
+                          (-1, +1, -1),
+                          (+1, -1, -1),
+                          (+1, +1, +1)}
 
         linear_energy_ranges = {v: (-2., 2.) for v in graph}
         quadratic_energy_ranges = {(u, v): (-1., 1.) for u, v in graph.edges}
@@ -38,3 +27,20 @@ class TestGeneration(unittest.TestCase):
                                                   (0, 1, 2),
                                                   linear_energy_ranges,
                                                   quadratic_energy_ranges)
+
+    def test_basic_noaux(self):
+        graph = nx.complete_graph(4)
+
+        configurations = {(-1, -1, -1, -1), (1, 1, 1, 1)}
+
+        linear_energy_ranges = {v: (-2., 2.) for v in graph}
+        quadratic_energy_ranges = {(u, v): (-1., 1.) for u, v in graph.edges}
+
+        h, J, offset, gap = maxgap.generate_ising(graph, configurations, list(graph),
+                                                  linear_energy_ranges,
+                                                  quadratic_energy_ranges)
+
+        print(h)
+        print(J)
+        print(offset)
+        print(gap)
