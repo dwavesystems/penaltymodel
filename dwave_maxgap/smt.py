47043e66e0c42d2a8583c068414a99b119512a12
Alexander Condello <acondello@dwavesys.com>
2017-10-23 14:54:20 -0700

    ok, things seem to be working, now need to fix this mess

--- dwave_maxgap/smt.py
+++ dwave_maxgap/smt.py
@@ -3,18 +3,29 @@
 import logging
 import logging.config
 import itertools
+from fractions import Fraction
 
 from six import iteritems, itervalues, moves
 
 import dwave_networkx as dnx
 
 from pysmt.shortcuts import Symbol, FreshSymbol, Real
-from pysmt.shortcuts import LE, GE, Plus, Times, Implies, Not
+from pysmt.shortcuts import LE, GE, Plus, Times, Implies, Not, And
 from pysmt.typing import REAL, BOOL
 
 
 logging.config.fileConfig('logging.conf')
 smtlog = logging.getLogger('smt')
+
+
+def limitReal(x, max_denominator=1000000):
+    """Creates an pysmt Real constant from x.
+
+    Casts x to the nearest fraction that has denominator at most
+    max_denominator.
+    """
+    f = Fraction(x).limit_denominator(max_denominator)
+    return Real((f.numerator, f.denominator))
 
 
 def allocate_gap():
@@ -26,16 +37,18 @@ def SpinTimes(spin, bias):
     cleaner log code as well as value checking.
 
     Args:
-        spin (int/float): -1.0 or 1.0
+        spin (int): -1 or 1
         bias (pysmt.shortcuts.Symbol): The bias
 
     Returns:
         spins * bias
 
     """
-    if spin == -1.:
-        return Times(Real(spin), bias)
-    elif spin == 1.:
+    if not isinstance(spin, int):
+        raise TypeError('spin must be an int')
+    if spin == -1:
+        return Times(Real((-1, 1)), bias)
+    elif spin == 1:
         # identity
         return bias
     else:
@@ -60,7 +73,7 @@ class Theta(object):
     """
     def __init__(self):
         self.assertions = set()
-        self.offset = Real(0.0)
+        self.offset = limitReal(0)
         self.linear = {}
         self.quadratic = {}
         self.adj = {}
@@ -79,8 +92,8 @@ class Theta(object):
 
             min_, max_ = linear_energy_ranges[v]
 
-            assertions.add(LE(bias, Real(max_)))
-            assertions.add(GE(bias, Real(min_)))
+            assertions.add(LE(bias, limitReal(max_)))
+            assertions.add(GE(bias, limitReal(min_)))
             smtlog.debug('{} <= {} <= {}'.format(min_, bias, max_))
 
             return bias
@@ -99,8 +112,8 @@ class Theta(object):
             else:
                 min_, max_ = quadratic_energy_ranges[(u, v)]
 
-            assertions.add(LE(bias, Real(max_)))
-            assertions.add(GE(bias, Real(min_)))
+            assertions.add(LE(bias, limitReal(max_)))
+            assertions.add(GE(bias, limitReal(min_)))
             smtlog.debug('{} <= {} <= {}'.format(min_, bias, max_))
 
             adj[u][v] = bias
@@ -122,18 +135,18 @@ class Theta(object):
         # gets added to the offset, otherwise it gets added to subtheta
         for v, bias in iteritems(self.linear):
             if v in spins:
-                subtheta.offset = Plus(subtheta.offset, Times(Real(spins[v]), bias))
+                subtheta.offset = Plus(subtheta.offset, Times(limitReal(spins[v]), bias))
             else:
                 subtheta.linear[v] = bias
 
         # and now the quadratic biases get allocated.
         for (u, v), bias in iteritems(self.quadratic):
             if u in spins and v in spins:
-                subtheta.offset = Plus(subtheta.offset, Times(Real(spins[v] * spins[u]), bias))
+                subtheta.offset = Plus(subtheta.offset, SpinTimes(spins[v] * spins[u], bias))
             elif u in spins:
-                subtheta.linear[v] = Plus(subtheta.linear[v], Times(Real(spins[u]), bias))
+                subtheta.linear[v] = Plus(subtheta.linear[v], SpinTimes(spins[u], bias))
             elif v in spins:
-                subtheta.linear[u] = Plus(subtheta.linear[u], Times(Real(spins[v]), bias))
+                subtheta.linear[u] = Plus(subtheta.linear[u], SpinTimes(spins[v], bias))
             else:
                 subtheta.quadratic[(u, v)] = bias
 
@@ -204,13 +217,13 @@ class Theta(object):
 #     return order, elimination_sets
 
 
-def _elimination_trees(graph, decision_variables):
+def _elimination_trees(theta, decision_variables):
     # auxiliary variables are any variables that are not decision
-    auxiliary_variables = set(n for n in graph if n not in decision_variables)
+    auxiliary_variables = set(n for n in theta.linear if n not in decision_variables)
 
     # get the adjacency of the auxiliary subgraph
-    adj = {v: {u for u in graph[v] if u in auxiliary_variables}
-           for v in graph if v in auxiliary_variables}
+    adj = {v: {u for u in theta.adj[v] if u in auxiliary_variables}
+           for v in theta.adj if v in auxiliary_variables}
 
     # get the elimination order that minimizes treewidth
     tw, order = dnx.treewidth_branch_and_bound(adj)
@@ -248,18 +261,23 @@ def _elimination_trees(graph, decision_variables):
 
 class Table(object):
     """TODO"""
-    def __init__(self, graph, decision_variables, theta):
+    def __init__(self, graph, decision_variables, linear_energy_ranges, quadratic_energy_ranges):
         # self.order, self.elimination_sets = _determine_elimination(graph, decision_variables)
 
-        self.trees, self.ancestors = _elimination_trees(graph, decision_variables)
+        smtlog.debug(';;; NEW TABLE')
 
-        self.theta = theta
+        self.theta = theta = Theta()
+        theta.build_from_graph(graph, linear_energy_ranges, quadratic_energy_ranges)
 
-        self.assertions = set()
+        self.trees, self.ancestors = _elimination_trees(theta, decision_variables)
+
+        self.assertions = theta.assertions
 
         self.fresh_auxvar = 0  # let's us make fresh aux variables
 
     def energy_upperbound(self, values):
+
+        smtlog.debug(';;; determining energy upper bound for {}'.format(values))
 
         subtheta = self.theta.fix_variables(values)
 
@@ -274,14 +292,17 @@ class Table(object):
             assert not subtheta.linear and not subtheta.quadratic
             return subtheta.offset
 
-    def energy(self, values):
+    def energy(self, values, break_aux_symmetry=True):
+        # NB: only break aux symmetry with symmetric energy ranges
+
+        smtlog.debug(';;; determining energy for {}'.format(values))
 
         subtheta = self.theta.fix_variables(values)
 
         # we need aux variables
         av = self.fresh_auxvar
         auxvars = {v: Symbol('aux{}_{}'.format(av, v), BOOL) for v in subtheta.linear}
-        if av == 0:
+        if break_aux_symmetry and av == 0:
             # without loss of generatlity, we can assume that the aux variables are all
             # spin-up for one configuration
             self.assertions.update(set(itervalues(auxvars)))
@@ -292,14 +313,15 @@ class Table(object):
 
         trees = self.trees
 
-        print(trees)
-
         if trees:
-            return Plus(self.message(trees, {}, subtheta, auxvars), subtheta.offset)
+            energy = Plus(self.message(trees, {}, subtheta, auxvars), subtheta.offset)
+            smtlog.debug(';;; energy == %s', energy)
+            return energy
         else:
             # if there are no variables to eliminate, then the offset of
             # subtheta is the exact value and we can just return it
             assert not subtheta.linear and not subtheta.quadratic
+            smtlog.debug(';;; energy == %s', subtheta.offset)
             return subtheta.offset
 
     def message(self, tree, spins, subtheta, auxvars):
@@ -319,33 +341,38 @@ class Table(object):
 
                 for u, bias in iteritems(subtheta.adj[v]):
                     if u in spins:
-                        yield Times(Real(spins[u]), bias)
+                        yield SpinTimes(spins[u], bias)
 
-            energy = Plus(energy_contributions())
+            plus_energy = Plus(energy_contributions())
+            minus_energy = SpinTimes(-1, plus_energy)
 
-            # if there are no more variables in the order, we can stop
-            # otherwise we need the next message variable
+            # if the variable has children, we need to recursively determine their energies
             if children:
-                spins[v] = 1.0
-                plus = self.message(children, spins, subtheta, auxvars)
-                spins[v] = -1.0
-                minus = self.message(children, spins, subtheta, auxvars)
+                # set v to be positive
+                spins[v] = 1
+                plus_energy = Plus(plus_energy, self.message(children, spins, subtheta, auxvars))
+                spins[v] = -1
+                minus_energy = Plus(minus_energy, self.message(children, spins, subtheta, auxvars))
                 del spins[v]
-            else:
-                plus = minus = Real(0.0)
 
             # we now need a real-valued smt variable to be our message
             m = FreshSymbol(REAL)
 
-            self.assertions.update({LE(m, Plus(energy, plus)),
-                                    LE(m, Plus(Times(energy, Real(-1.)), minus)),
-                                    Implies(aux, GE(m, Plus(energy, plus))),
-                                    Implies(Not(aux), GE(m, Plus(Times(energy, Real(-1.)), minus)))
+            ancestor_aux = {auxvars[u] if spins[u] > 0 else Not(auxvars[u])
+                            for u in self.ancestors[v]}
+            plus_aux = And({aux}.union(ancestor_aux))
+            minus_aux = And({Not(aux)}.union(ancestor_aux))
+
+            self.assertions.update({LE(m, plus_energy),
+                                    LE(m, minus_energy),
+                                    Implies(plus_aux, GE(m, plus_energy)),
+                                    Implies(minus_aux, GE(m, minus_energy))
                                     })
-            smtlog.debug('%s <= %s', m, Plus(energy, plus))
-            smtlog.debug('%s <= %s', m, Plus(Times(energy, Real(-1.)), minus))
-            smtlog.debug('%s implies %s >= %s', aux, m, Plus(energy, plus))
-            smtlog.debug('%s implies %s >= %s', Not(aux), m, Plus(Times(energy, Real(-1.)), minus))
+            smtlog.debug(';;; v={}, message={}, fixed={}'.format(v, m, spins))
+            smtlog.debug('%s <= %s', m, plus_energy)
+            smtlog.debug('%s <= %s', m, minus_energy)
+            smtlog.debug('%s implies %s >= %s', plus_aux, m, plus_energy)
+            smtlog.debug('%s implies %s >= %s', minus_aux, m, minus_energy)
 
             energy_sources.add(m)
 
@@ -366,7 +393,7 @@ class Table(object):
 
                 for u, bias in iteritems(subtheta.adj[v]):
                     if u in spins:
-                        yield Times(Real(spins[u]), bias)
+                        yield Times(limitReal(spins[u]), bias)
 
             energy = Plus(energy_contributions())
 
@@ -379,15 +406,15 @@ class Table(object):
                 minus = self.message_upperbound(subtree, spins, subtheta)
                 del spins[v]
             else:
-                plus = minus = Real(0.0)
+                plus = minus = limitReal(0.0)
 
             # we now need a real-valued smt variable to be our message
             m = FreshSymbol(REAL)
 
             self.assertions.update({LE(m, Plus(energy, plus)),
-                                    LE(m, Plus(Times(energy, Real(-1.)), minus))})
+                                    LE(m, Plus(Times(energy, limitReal(-1.)), minus))})
             smtlog.debug('%s <= %s', m, Plus(energy, plus))
-            smtlog.debug('%s <= %s', m, Plus(Times(energy, Real(-1.)), minus))
+            smtlog.debug('%s <= %s', m, Plus(Times(energy, limitReal(-1.)), minus))
 
             energy_sources.add(m)
 
