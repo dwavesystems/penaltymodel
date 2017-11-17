e22e6a890563909d08b9eab09f40c823905184a7
Alexander Condello <acondello@dwavesys.com>
2017-11-16 17:31:37 -0800

    Remove extra file

--- penaltymodel/interface.py
+++
@@ -1,9 +0,0 @@
-from penaltymodel.plugins import factories
-
-
-def get_penalty_model(graph, decision_variables, constraint):
-    for factory in factories:
-        pm = factory(graph, decision_variables, constraint)
-
-        print(pm)
-
