6960fa2344720c60714a7b492bb66f43875786ce
Alexander Condello <acondello@dwavesys.com>
2017-12-15 18:01:08 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-from penaltymodel_maxgap.packge_info import __version__
+from penaltymodel_maxgap.packge_info import __version__, __author__, __description__, __authoremail__
 
-install_requires = []
+install_requires = ['six', 'dwave_networkx>=0.6.0', 'pysmt', 'penaltymodel>=0.9.1']
 extras_require = {}
 
 packages = ['penaltymodel_maxgap']
@@ -10,6 +10,9 @@ packages = ['penaltymodel_maxgap']
 setup(
     name='penaltymodel_maxgap',
     version=__version__,
+    author=__author__,
+    author_email=__authoremail__,
+    description=__description__,
     license='Apache 2.0',
     packages=packages,
     install_requires=install_requires,
