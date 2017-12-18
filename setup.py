472451cbb3a0c2d257943d4049ea7704351edbc5
Alexander Condello <acondello@dwavesys.com>
2017-12-18 15:33:40 -0800

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-from penaltymodel.package_info import __version__, __author__, __description__, __authoremail__
+from penaltymodel_cache.package_info import __version__, __author__, __description__, __authoremail__
 
 install_requires = ['penaltymodel>=0.9.1',
                     'six',
