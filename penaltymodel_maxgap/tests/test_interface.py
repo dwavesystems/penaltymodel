d0c8c0416dd919e148c4b195d56066d92f0540f6
Alexander Condello <acondello@dwavesys.com>
2017-12-19 10:05:42 -0800

    Fix bug for specification with vartype set to BINARY

--- penaltymodel_maxgap/tests/test_interface.py
+++ penaltymodel_maxgap/tests/test_interface.py
@@ -1,8 +1,8 @@
 import unittest
+import itertools
 
 import networkx as nx
-
-from penaltymodel import Specification
+import penaltymodel as pm
 
 import penaltymodel_maxgap as maxgap
 
@@ -12,12 +12,43 @@ class TestInterface(unittest.TestCase):
     Test that the interface gives a penalty model corresponding to the specification"""
     def test_typical(self):
         graph = nx.complete_graph(3)
-        spec = Specification(graph, [0, 1], {(-1, -1): 0, (+1, +1): 0})
+        spec = pm.Specification(graph, [0, 1], {(-1, -1): 0, (+1, +1): 0})
 
-        pm = maxgap.get_penalty_model(spec)
+        widget = maxgap.get_penalty_model(spec)
 
         # some quick test to see that the penalty model propogated in
         for v in graph:
-            self.assertIn(v, pm.model.linear)
+            self.assertIn(v, widget.model.linear)
         for (u, v) in graph.edges:
-            self.assertIn(u, pm.model.adj[v])
+            self.assertIn(u, widget.model.adj[v])
+
+    def test_binary_specification(self):
+        graph = nx.Graph()
+        for i in range(4):
+            for j in range(4, 8):
+                graph.add_edge(i, j)
+
+        decision_variables = (0, 1)
+        feasible_configurations = ((0, 0), (1, 1))  # equality
+
+        spec = pm.Specification(graph, decision_variables, feasible_configurations, vartype=pm.BINARY)
+        widget = maxgap.get_penalty_model(spec)
+
+        self.assertIs(widget.model.vartype, pm.BINARY)
+
+        # test the correctness of the widget
+        energies = {}
+        for decision_config in itertools.product((0, 1), repeat=2):
+            energies[decision_config] = float('inf')
+
+            for aux_config in itertools.product((0, 1), repeat=6):
+                sample = dict(enumerate(decision_config + aux_config))
+                energy = widget.model.energy(sample)
+
+                energies[decision_config] = min(energies[decision_config], energy)
+
+        for decision_config, energy in energies.items():
+            if decision_config in feasible_configurations:
+                self.assertAlmostEqual(energy, widget.ground_energy)
+            else:
+                self.assertGreaterEqual(energy, widget.ground_energy + widget.classical_gap)
