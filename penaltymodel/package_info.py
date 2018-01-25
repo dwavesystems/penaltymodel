77228a9e484e7a184ad3c2bcccdedd1ec8d4a265
Alexander Condello <acondello@dwavesys.com>
2018-01-25 15:49:21 -0800

    Update version to 0.12.0

--- penaltymodel/package_info.py
+++ penaltymodel/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0'
+__version__ = '0.12.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Utilities and interfaces for using penalty models.'
