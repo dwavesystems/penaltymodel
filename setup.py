767844f7c2b151c1de928d7fd5cbed1c967b3946
Alexander Condello <acondello@dwavesys.com>
2018-03-09 11:17:25 -0800

    Correct requirement upper bound syntax

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 from penaltymodel.package_info import __version__, __author__, __description__, __authoremail__
 
-install_requires = ['dimod>=0.5.0<0.6.0',
-                    'six>=1.11.0<2.0.0',
-                    'networkx>=2.0<3.0',
-                    'enum34>=1.1.6<2.0.0']
-extras_require = {'all': ['penaltymodel_cache>=0.1.0<0.2.0',
-                          'penaltymodel_maxgap>=0.1.0<0.2.0']}
+install_requires = ['dimod>=0.5.0,<0.6.0',
+                    'six>=1.11.0,<2.0.0',
+                    'networkx>=2.0,<3.0',
+                    'enum34>=1.1.6,<2.0.0']
+extras_require = {'all': ['penaltymodel_cache>=0.1.0,<0.2.0',
+                          'penaltymodel_maxgap>=0.1.0,<0.2.0']}
 
 packages = ['penaltymodel',
             'penaltymodel.classes']
