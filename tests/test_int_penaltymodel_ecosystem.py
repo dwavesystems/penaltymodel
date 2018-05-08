5c9fff2443d7a752151298b4a9dc1a3da3bf074d
Alexander Condello <acondello@dwavesys.com>
2018-05-08 13:10:26 -0700

    Move integration tests to top level

--- tests/test_int_penaltymodel_ecosystem.py
+++ tests/test_int_penaltymodel_ecosystem.py
@@ -2,29 +2,19 @@ import unittest
 
 import networkx as nx
 
+import dimod
+
 import penaltymodel as pm
-
-try:
-    import penaltymodel_cache as pmc
-    _cache = True
-except ImportError:
-    _cache = False
+import penaltymodel_cache as pmc
+import penaltymodel_maxgap as maxgap
 
 
-try:
-    import penaltymodel_maxgap as maxgap
-    _maxgap = True
-except ImportError:
-    _maxgap = False
-
-
-@unittest.skipUnless(_cache, "penaltymodel_cache is not installed")
 class TestInterfaceWithCache(unittest.TestCase):
     def test_retrieval(self):
         # put some stuff in the database
 
         spec = pm.Specification(nx.path_graph(2), (0, 1), {(-1, -1), (1, 1)}, vartype=pm.SPIN)
-        model = pm.BinaryQuadraticModel({0: 0, 1: 0}, {(0, 1): -1}, 0.0, vartype=pm.SPIN)
+        model = dimod.BinaryQuadraticModel({0: 0, 1: 0}, {(0, 1): -1}, 0.0, vartype=pm.SPIN)
         widget = pm.PenaltyModel.from_specification(spec, model, 2, -1)
 
         for cache in pm.iter_caches():
@@ -36,10 +26,12 @@ class TestInterfaceWithCache(unittest.TestCase):
         self.assertEqual(widget, new_widget)
 
 
-@unittest.skipUnless(_maxgap, "penaltymodel_maxgap is not installed")
 class TestInterfaceWithMaxGap(unittest.TestCase):
     def test_retrieval(self):
-        spec = pm.Specification(nx.path_graph(2), (0, 1), {(-1, -1), (1, 1)}, vartype=pm.SPIN)
+
+        eq = {(-1, -1), (1, 1)}
+
+        spec = pm.Specification(nx.path_graph(2), (0, 1), eq, vartype=pm.SPIN)
         widget = pm.get_penalty_model(spec)
 
         self.assertEqual(widget.model.linear, {0: 0, 1: 0})
