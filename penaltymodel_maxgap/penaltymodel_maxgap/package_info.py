9749ab9d1dd755586ab15643bf73056a252f9f25
Alexander Condello <acondello@dwavesys.com>
2018-05-30 16:31:13 -0700

    Update version 0.3.1 -> 0.3.2

--- penaltymodel_maxgap/penaltymodel_maxgap/package_info.py
+++ penaltymodel_maxgap/penaltymodel_maxgap/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Generates penalty models using smt solvers.'
