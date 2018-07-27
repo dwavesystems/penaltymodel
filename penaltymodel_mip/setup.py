e48d15766c840e5000bb5a86774603615e6bee5b
Alexander Condello <acondello@dwavesys.com>
2018-07-27 16:06:21 -0700

    Fix bug in setup.py preventing ortools install

--- penaltymodel_mip/setup.py
+++ penaltymodel_mip/setup.py
@@ -12,7 +12,7 @@ else:
     exec(open("./penaltymodel/mip/package_info.py").read())
 
 install_requires = ['dimod>=0.6.0,<0.7.0',
-                    'networkx>=2.0,<3.0'
+                    'networkx>=2.0,<3.0',
                     'ortools>=6.6.4659,<7.0.0',
                     'penaltymodel>=0.15.0,<0.16.0',
                     ]
