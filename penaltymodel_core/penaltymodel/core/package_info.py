dd8dd07d5564356c480abb6d792b01ff613fe974
Alexander Condello <acondello@dwavesys.com>
2018-07-31 09:27:47 -0700

    Update core version 0.15.1 -> 0.15.2

--- penaltymodel_core/penaltymodel/core/package_info.py
+++ penaltymodel_core/penaltymodel/core/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.15.1'
+__version__ = '0.15.2'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Utilities and interfaces for using penalty models.'
