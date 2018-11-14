5eaa462bbe010acea9565ae6a0ccefe0cd016e80
m3ller <mwong@dwavesys.com>
2018-11-14 15:37:33 -0800

    Make and-gate test into an ising test. Boolean-and does not completely work for spin values, so replace bool-and with min(a,b)

--- penaltymodel_toy/lp_test.py
+++ penaltymodel_toy/lp_test.py
@@ -26,24 +26,24 @@ class TestLinearProgramming(unittest.TestCase):
 
     def test_and_gate_ising(self):
         min_gap = 2
-        and_gate_values = {(0, 0, 0), (0, 1, 0), (1, 0, 0), (1, 1, 1)}
+        and_gate_values = {(-1, -1, -1), (-1, 1, -1), (1, -1, -1), (1, 1, 1)}
 
         # Make a BQM for an and-gate
-        csp = dbc.ConstraintSatisfactionProblem(dbc.BINARY)
+        csp = dbc.ConstraintSatisfactionProblem(dbc.SPIN)
         csp.add_constraint(and_gate_values, ('a', 'b', 'c'))
         bqm = dbc.stitch(csp, min_classical_gap=min_gap)
 
         # Check that valid or-gate inputs are at ground; invalid values meet threshold requirement
-        ground_energy = bqm.energy({'a': 0, 'b': 0, 'c': 0})    # Valid or-gate value
-        for a, b, c in product([0, 1], repeat=3):
+        ground_energy = bqm.energy({'a': -1, 'b': -1, 'c': -1})    # Valid or-gate value
+        for a, b, c in product([-1, 1], repeat=3):
             energy = bqm.energy({'a': a, 'b': b, 'c': c})
 
-            if c == (a and b):
+            if c == min(a, b):
                 # and-gate values
                 self.assertEqual(energy, ground_energy)
             else:
                 # and-or-gate values
-                self.assertGreaterEqual(energy, ground_energy + min_gap)
+                self.assertGreaterEqual(energy, ground_energy + min_gap, "a:{}, b:{}, c:{}".format(a, b, c))
 
 
 if __name__ == "__main__":
