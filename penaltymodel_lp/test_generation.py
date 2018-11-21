158a5c7decbbae4d4ba33847a2899d234891931e
m3ller <mwong@dwavesys.com>
2018-11-21 13:41:57 -0800

    Add verify_gate_bqm(..) to help with verifying bqms for gates

--- penaltymodel_lp/test_generation.py
+++ penaltymodel_lp/test_generation.py
@@ -8,42 +8,52 @@ import penaltymodel.lp as lp
 #TODO: need to run without truth table
 #TODO: test with binary values
 class TestLinearProgramming(unittest.TestCase):
+    def verify_gate_bqm(self, bqm, nodes, get_gate_output, min_gap=2):
+        """Check that valid gate inputs are at ground; invalid values meet threshold (min_gap) requirement
+        """
+        for a, b, c in product([-1, 1], repeat=3):
+            spin_state = {nodes[0]: a, nodes[1]: b, nodes[2]: c}
+            energy = bqm.energy(spin_state)
+
+            if c == get_gate_output(a, b):
+                self.assertEqual(energy, 0, "Failed for {}".format(spin_state))
+            else:
+                self.assertGreaterEqual(energy, min_gap, "Failed for {}".format(spin_state))
+
     def test_empty(self):
         pass
 
-    def test_dictionary_inputs(self):
-        min_gap = 2
-        or_gate_values = {(-1, 1, 1): 0, (1, -1, 1): 0, (1, 1, 1): 0, (-1, -1, -1): 0}
-
-        # Make a BQM for an or-gate
+    def test_dictionary_input(self):
+        # Make or-gate BQM
         nodes = ['a', 'b', 'c']
+        or_gate_values = {(-1, 1, 1): 0, (1, -1, 1): 0, (1, 1, 1): 0, (-1, -1, -1): 0}
         bqm, gap = lp.generate_bqm(nx.complete_graph(nodes), or_gate_values, nodes)
 
-        # Check that valid or-gate inputs are at ground; invalid values meet threshold requirement
-        ground_energy = bqm.energy({'a': -1, 'b': -1, 'c': -1})    # Valid or-gate value
-        for a, b, c in product([-1, 1], repeat=3):
-            energy = bqm.energy({'a': a, 'b': b, 'c': c})
+        self.verify_gate_bqm(bqm, nodes, max)
 
-            if c == max(a, b):
-                # Or-gate values
-                self.assertEqual(energy, ground_energy, "Failed for a:{}, b:{}, c:{}".format(a, b, c))
-            else:
-                # Non-or-gate values
-                self.assertGreaterEqual(energy, ground_energy + min_gap, "Failed for a:{}, b:{}, c:{}".format(a, b, c))
-
-    def test_iterable_inputs(self):
-        min_gap = 2
-        and_gate_set = {(-1, -1, -1), (-1, 1, -1), (1, -1, -1), (1, 1, 1)}
+    def test_set_input(self):
+        # Generate BQM for a set
         nodes = [1, 2, 3]
-        bqm, gap = lp.generate_bqm(nx.complete_graph(nodes), and_gate_set, nodes)
+        and_gate_set = {(-1, -1, -1), (-1, 1, -1), (1, -1, -1), (1, 1, 1)}
+        bqm_set, gap_set = lp.generate_bqm(nx.complete_graph(nodes), and_gate_set, nodes)
+
+        self.verify_gate_bqm(bqm_set, nodes, min)
+
+    def test_list_input(self):
+        # Generate BQM for a list
+        nodes = [1, 2, 3]
+        nand_gate_list = [(-1, -1, 1), (-1, 1, 1), (1, -1, 1), (1, 1, -1)]
+        bqm_list, gap_list = lp.generate_bqm(nx.complete_graph(nodes), nand_gate_list, nodes)
+
+        self.verify_gate_bqm(bqm_list, nodes, lambda x, y: -1 * min(x, y))
 
     def test_multi_energy_bqm(self):
+        # Create BQM for fully determined configuration with no ground states
         configurations = {(-1, -1): -.5, (-1, 1): 3.5, (1, -1): 1.5, (1, 1): 3.5}
         nodes = ['x', 'y']
-
         bqm, gap = lp.generate_bqm(nx.complete_graph(nodes), configurations, nodes)
 
-        # Verify bqm
+        # Verify BQM
         for (x, y), expected_energy in configurations.items():
             energy = bqm.energy({'x': x, 'y': y})
             self.assertEqual(energy, expected_energy, "Failed for x:{}, y:{}".format(x, y))
