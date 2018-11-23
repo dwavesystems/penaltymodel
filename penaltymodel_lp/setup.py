7b76b99a7041315c33b9ef2308cb66f630992a16
m3ller <mwong@dwavesys.com>
2018-11-22 16:07:18 -0800

    Add install_requires and packages to setup.py

--- penaltymodel_lp/setup.py
+++ penaltymodel_lp/setup.py
@@ -2,8 +2,19 @@ from setuptools import setup
 
 FACTORY_ENTRYPOINT = 'penaltymodel_factory'
 
+install_requires = ['dimod>=0.6.0,<0.8.0',
+                    'penaltymodel>=0.15.0,<0.16.0',
+                    'scipy>=0.15.0,<1.1.0',
+                    'numpy>=0.0.0,<1.16.0'
+                    ]
+
+packages = ['penaltymodel',
+            'penaltymodel.lp',
+            ]
+
 setup(
     name="penaltymodel-lp",
+    install_requires=install_requires,
     entry_points={
         FACTORY_ENTRYPOINT: ['lp = penaltymodel.lp:get_penalty_model']
     }
