786ee43727fd00b6a4add6c4badff7204f0be3c8
m3ller <mwong@dwavesys.com>
2019-03-05 16:48:35 -0800

    Add additional set up information. Based on mip's setup.py

--- penaltymodel_lp/setup.py
+++ penaltymodel_lp/setup.py
@@ -1,4 +1,14 @@
+from __future__ import absolute_import
+
+import sys
 from setuptools import setup
+
+_PY2 = sys.version_info.major == 2
+
+if _PY2:
+    execfile("./penaltymodel/lp/package_info.py")
+else:
+    exec(open("./penaltymodel/lp/package_info.py").read())
 
 FACTORY_ENTRYPOINT = 'penaltymodel_factory'
 
@@ -12,11 +22,34 @@ packages = ['penaltymodel',
             'penaltymodel.lp',
             ]
 
+classifiers = [
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    ]
+
+python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*'
+
 setup(
     name="penaltymodel-lp",
-    install_requires=install_requires,
+    version=__version__,
+    author=__author__,
+    author_email=__authoremail__,
+    description=__description__,
+    long_description=open('README.rst').read(),
+    url='https://github.com/dwavesystems/penaltymodel',
+    license='Apache 2.0',
     packages=packages,
-    entry_points={
-        FACTORY_ENTRYPOINT: ['lp = penaltymodel.lp:get_penalty_model']
-    }
+    classifiers=classifiers,
+    python_requires=python_requires,
+    install_requires=install_requires,
+    entry_points={FACTORY_ENTRYPOINT: ['lp = penaltymodel.lp:get_penalty_model']},
+    zip_safe=False
 )
