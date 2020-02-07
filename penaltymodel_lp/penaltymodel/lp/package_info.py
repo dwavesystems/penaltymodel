f23c637b79a96e1d8d18edc79968a525e4e1c63b
m3ller <mwong@dwavesys.com>
2020-02-07 15:13:00 -0800

    Update lp version 0.1.1 -> 0.1.2
    Fixes
    -----
    - increases the package version ranges of numpy and dimod

--- penaltymodel_lp/penaltymodel/lp/package_info.py
+++ penaltymodel_lp/penaltymodel/lp/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'mwong@dwavesys.com'
 __description__ = "Generates penalty models using SciPy's linear programming."
