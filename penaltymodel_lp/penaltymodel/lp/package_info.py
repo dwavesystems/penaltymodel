ea6aea82b1ad7980fcb958e5327bbe3962196ece
m3ller <melcanwong@hotmail.com>
2020-02-11 09:22:32 -0800

    Update lp version 0.1.2 -> 0.1.3
    Features
    --------
    - py 3.8 support

--- penaltymodel_lp/penaltymodel/lp/package_info.py
+++ penaltymodel_lp/penaltymodel/lp/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'mwong@dwavesys.com'
 __description__ = "Generates penalty models using SciPy's linear programming."
