975c977fbfa77552c9b6b7bae048a5c07e073da5
m3ller <melcanwong@hotmail.com>
2020-02-10 17:31:06 -0800

    Add more elegant approach to toggling behaviour

--- penaltymodel_lp/penaltymodel/lp/generation.py
+++ penaltymodel_lp/penaltymodel/lp/generation.py
@@ -169,19 +169,15 @@ def generate_bqm(graph, table, decision_variables,
     #   failing early and getting a reliable solution from another penaltymodel than from
     #   simply suppressing the non-full-row-rank matrix warning.
     # TODO: address warnings by preconditioning the matrix and factorizing the matrix
-    if catch_warnings:
-        with warnings.catch_warnings():
-            warnings.filterwarnings("error")
-            try:
-                result = linprog(cost_weights.flatten(), A_eq=noted_matrix, b_eq=noted_bound,
-                                 A_ub=unnoted_matrix, b_ub=unnoted_bound, bounds=bounds)
-            except (OptimizeWarning, LinAlgWarning) as e:
-                # If catch_warnings is False, we force penaltymodel-lp to keep going
-                # rather than to fail and pass to the next penaltymodel
-                    raise ValueError('Penaltymodel-lp has a bad matrix')
-    else:
-        result = linprog(cost_weights.flatten(), A_eq=noted_matrix, b_eq=noted_bound,
-                         A_ub=unnoted_matrix, b_ub=unnoted_bound, bounds=bounds)
+    warning_filter = "error" if catch_warnings else "default"
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings(warning_filter)
+        try:
+            result = linprog(cost_weights.flatten(), A_eq=noted_matrix, b_eq=noted_bound,
+                             A_ub=unnoted_matrix, b_ub=unnoted_bound, bounds=bounds)
+        except (OptimizeWarning, LinAlgWarning) as e:
+                raise ValueError('Penaltymodel-lp has a bad matrix')
 
     # Unable to find a solution
     if not result.success:
