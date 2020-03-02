3f286355384665ee76fc9ae24e401f892df41752
m3ller <mwong@dwavesys.com>
2020-03-02 11:08:29 -0800

    Update maxgap version 0.5.2 -> 0.5.3
    Features
    --------
    - Bump up pysmt to 0.8.0

--- penaltymodel_maxgap/penaltymodel/maxgap/package_info.py
+++ penaltymodel_maxgap/penaltymodel/maxgap/package_info.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Generates penalty models using smt solvers.'
