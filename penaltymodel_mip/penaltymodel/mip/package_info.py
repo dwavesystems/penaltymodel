a5617204e83e378cc95dc0ca9df1665aae7958e7
m3ller <mwong@dwavesys.com>
2020-04-02 17:00:25 -0700

    Update mip version 0.2.2 -> 0.2.3
    Fixes
    -----
    - Now handles the case when all decision configurations are defined

--- penaltymodel_mip/penaltymodel/mip/package_info.py
+++ penaltymodel_mip/penaltymodel/mip/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = "Generates penalty models using Google Optimization Tools' Mixed-Integer Programming capability."
