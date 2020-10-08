3dc2daf334c9b748bd06f55e3de3578a837490a9
Heidi Tong <htong@dwavesys.com>
2020-10-08 11:04:48 -0700

    Use TemporaryDirectory() as context manager

--- tests/test_int_penaltymodel_ecosystem.py
+++ tests/test_int_penaltymodel_ecosystem.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest import mock
 import tempfile
+import os.path as path
 
 import networkx as nx
 
@@ -10,21 +11,22 @@ import penaltymodel.core as pm
 
 class TestInterfaceWithCache(unittest.TestCase):
     def test_retrieval(self):
-        tmp_db_file = tempfile.NamedTemporaryFile().name
+        with tempfile.TemporaryDirectory() as tmpdir:         
+            filename = path.join(tmpdir, 'tmp_db_file')
 
-        with mock.patch("penaltymodel.cache.database_manager.cache_file", lambda: tmp_db_file):
-            # put some stuff in the database
-            spec = pm.Specification(nx.path_graph(2), (0, 1), {(-1, -1), (1, 1)}, vartype=pm.SPIN)
-            model = dimod.BinaryQuadraticModel({0: 0, 1: 0}, {(0, 1): -1}, 0.0, vartype=pm.SPIN)
-            widget = pm.PenaltyModel.from_specification(spec, model, 2, -1)
+            with mock.patch("penaltymodel.cache.database_manager.cache_file", lambda: filename):
+                # put some stuff in the database
+                spec = pm.Specification(nx.path_graph(2), (0, 1), {(-1, -1), (1, 1)}, vartype=pm.SPIN)
+                model = dimod.BinaryQuadraticModel({0: 0, 1: 0}, {(0, 1): -1}, 0.0, vartype=pm.SPIN)
+                widget = pm.PenaltyModel.from_specification(spec, model, 2, -1)
 
-            for cache in pm.iter_caches():
-                cache(widget)
+                for cache in pm.iter_caches():
+                    cache(widget)
 
-            # now try to get it back
-            new_widget = pm.get_penalty_model(spec)
+                # now try to get it back
+                new_widget = pm.get_penalty_model(spec)
 
-            self.assertEqual(widget, new_widget)
+                self.assertEqual(widget, new_widget)
 
 class TestInterfaceWithLP(unittest.TestCase):
     def assert_dict_almost_equal(self, dict0, dict1):
@@ -32,13 +34,14 @@ class TestInterfaceWithLP(unittest.TestCase):
             self.assertAlmostEqual(dict0[key], dict1[key])
 
     def test_retrieval(self):
-        tmp_db_file = tempfile.NamedTemporaryFile().name
+        with tempfile.TemporaryDirectory() as tmpdir:         
+            filename = path.join(tmpdir, 'tmp_db_file')
 
-        with mock.patch("penaltymodel.cache.database_manager.cache_file", lambda: tmp_db_file):
-            eq = {(-1, -1), (1, 1)}
+            with mock.patch("penaltymodel.cache.database_manager.cache_file", lambda: filename):
+                eq = {(-1, -1), (1, 1)}
 
-            spec = pm.Specification(nx.path_graph(2), (0, 1), eq, vartype=pm.SPIN)
-            widget = pm.get_penalty_model(spec)
+                spec = pm.Specification(nx.path_graph(2), (0, 1), eq, vartype=pm.SPIN)
+                widget = pm.get_penalty_model(spec)
 
-            self.assert_dict_almost_equal(widget.model.linear, {0: 0, 1: 0})
-            self.assert_dict_almost_equal(widget.model.quadratic, {(0, 1): -1})
+                self.assert_dict_almost_equal(widget.model.linear, {0: 0, 1: 0})
+                self.assert_dict_almost_equal(widget.model.quadratic, {(0, 1): -1})
