e34080ac93a1296e377e77f773c7f02944bdff34
Heidi Tong <htong@dwavesys.com>
2020-10-08 11:41:25 -0700

    Add decorator for isolating cache

--- tests/test_int_penaltymodel_ecosystem.py
+++ tests/test_int_penaltymodel_ecosystem.py
@@ -9,24 +9,33 @@ import dimod
 
 import penaltymodel.core as pm
 
-class TestInterfaceWithCache(unittest.TestCase):
-    def test_retrieval(self):
+def isolated_cache(f):
+    """Method decorator used to isolate the method's penaltymodel cache."""
+    def _isolated_cache(obj):
         with tempfile.TemporaryDirectory() as tmpdir:         
-            filename = path.join(tmpdir, 'tmp_db_file')
+            filename = path.join(tmpdir, "tmp_db_file")
 
             with mock.patch("penaltymodel.cache.database_manager.cache_file", lambda: filename):
-                # put some stuff in the database
-                spec = pm.Specification(nx.path_graph(2), (0, 1), {(-1, -1), (1, 1)}, vartype=pm.SPIN)
-                model = dimod.BinaryQuadraticModel({0: 0, 1: 0}, {(0, 1): -1}, 0.0, vartype=pm.SPIN)
-                widget = pm.PenaltyModel.from_specification(spec, model, 2, -1)
+                f(obj)
 
-                for cache in pm.iter_caches():
-                    cache(widget)
+    return _isolated_cache
 
-                # now try to get it back
-                new_widget = pm.get_penalty_model(spec)
+class TestInterfaceWithCache(unittest.TestCase):
+    @isolated_cache
+    def test_retrieval(self):
+        # put some stuff in the database
 
-                self.assertEqual(widget, new_widget)
+        spec = pm.Specification(nx.path_graph(2), (0, 1), {(-1, -1), (1, 1)}, vartype=pm.SPIN)
+        model = dimod.BinaryQuadraticModel({0: 0, 1: 0}, {(0, 1): -1}, 0.0, vartype=pm.SPIN)
+        widget = pm.PenaltyModel.from_specification(spec, model, 2, -1)
+
+        for cache in pm.iter_caches():
+            cache(widget)
+
+        # now try to get it back
+        new_widget = pm.get_penalty_model(spec)
+
+        self.assertEqual(widget, new_widget)
 
 class TestInterfaceWithLP(unittest.TestCase):
     def assert_dict_almost_equal(self, dict0, dict1):
@@ -34,15 +43,12 @@ class TestInterfaceWithLP(unittest.TestCase):
         for key in dict0:
             self.assertAlmostEqual(dict0[key], dict1[key])
 
+    @isolated_cache
     def test_retrieval(self):
-        with tempfile.TemporaryDirectory() as tmpdir:         
-            filename = path.join(tmpdir, 'tmp_db_file')
+        eq = {(-1, -1), (1, 1)}
 
-            with mock.patch("penaltymodel.cache.database_manager.cache_file", lambda: filename):
-                eq = {(-1, -1), (1, 1)}
+        spec = pm.Specification(nx.path_graph(2), (0, 1), eq, vartype=pm.SPIN)
+        widget = pm.get_penalty_model(spec)
 
-                spec = pm.Specification(nx.path_graph(2), (0, 1), eq, vartype=pm.SPIN)
-                widget = pm.get_penalty_model(spec)
-
-                self.assert_dict_almost_equal(widget.model.linear, {0: 0, 1: 0})
-                self.assert_dict_almost_equal(widget.model.quadratic, {(0, 1): -1})
+        self.assert_dict_almost_equal(widget.model.linear, {0: 0, 1: 0})
+        self.assert_dict_almost_equal(widget.model.quadratic, {(0, 1): -1})
