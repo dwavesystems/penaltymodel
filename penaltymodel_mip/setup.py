3652e21617bd6e304a555558e6708966b4dda8d0
Heidi Tong <htong@dwavesys.com>
2020-12-11 10:36:37 -0800

    Add py39 tests

--- penaltymodel_mip/setup.py
+++ penaltymodel_mip/setup.py
@@ -8,7 +8,7 @@ exec(open("./penaltymodel/mip/package_info.py").read())
 
 install_requires = ['dimod>=0.6.0,<0.10.0',
                     'networkx>=2.0,<3.0',
-                    'ortools>=6.6.4659,<8.0.0',
+                    'ortools>=6.6.4659,<9.0.0',
                     'penaltymodel>=0.16.0,<0.17.0',
                     ]
 
@@ -26,6 +26,7 @@ classifiers = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
     ]
 
 python_requires = '>=3.5'
