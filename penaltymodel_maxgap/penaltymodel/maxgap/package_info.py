36ed93647a11fc4beb2fb96e62e3f11a63a58b6f
Heidi Tong <htong@dwavesys.com>
2020-12-15 13:04:08 -0800

    Release maxgap version 0.5.4
    New Features
    ---
    - Python 3.9 support

--- penaltymodel_maxgap/penaltymodel/maxgap/package_info.py
+++ penaltymodel_maxgap/penaltymodel/maxgap/package_info.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Generates penalty models using smt solvers.'
