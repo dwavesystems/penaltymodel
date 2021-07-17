e81db884cd07bf8e57819db65b4569d34006df01
Alexander Condello <arcondello@gmail.com>
2021-07-17 12:37:13 -0700

    Update version 0.16.4 -> 0.16.5
    New Features
    ------------
    * Support `dimod>=0.8.0,<0.11.0`

--- penaltymodel_core/penaltymodel/core/package_info.py
+++ penaltymodel_core/penaltymodel/core/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.16.4'
+__version__ = '0.16.5'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Utilities and interfaces for using penalty models.'
