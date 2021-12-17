69c9d799b04008df45048fc1272af31cd191084d
Alexander Condello <arcondello@gmail.com>
2021-12-17 13:58:04 -0800

    Update package versions
    penaltymodel-cache 0.4.4 -> 0.4.5
    penaltymodel-core 0.16.5 -> 0.16.6
    penaltymodel-lp 0.1.5 -> 0.1.6
    penaltymodel-maxgap 0.5.5 -> 0.5.6
    penaltymodel-mip 0.2.5 -> 0.2.6

--- penaltymodel_lp/penaltymodel/lp/package_info.py
+++ penaltymodel_lp/penaltymodel/lp/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'mwong@dwavesys.com'
 __description__ = "Generates penalty models using SciPy's linear programming."
--- penaltymodel_maxgap/penaltymodel/maxgap/package_info.py
+++ penaltymodel_maxgap/penaltymodel/maxgap/package_info.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-__version__ = '0.5.5'
+__version__ = '0.5.6'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Generates penalty models using smt solvers.'
