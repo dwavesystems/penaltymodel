c8e1f01dae5073f39b73cb61d15c32de0105698e
Alexander Condello <arcondello@gmail.com>
2021-12-17 13:58:04 -0800

    Update package versions
    penaltymodel-cache 0.4.4 -> 0.4.5
    penaltymodel-core 0.16.5 -> 0.16.6
    penaltymodel-lp 0.1.5 -> 0.1.6
    penaltymodel-maxgap 0.5.5 -> 0.5.6
    penaltymodel-mip 0.2.5 -> 0.2.6

--- penaltymodel_core/penaltymodel/core/package_info.py
+++ penaltymodel_core/penaltymodel/core/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.16.5'
+__version__ = '0.16.6'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'Utilities and interfaces for using penalty models.'
--- penaltymodel_mip/penaltymodel/mip/package_info.py
+++ penaltymodel_mip/penaltymodel/mip/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = "Generates penalty models using Google Optimization Tools' Mixed-Integer Programming capability."
